81ea34aaac8d5ff05b5ddfb47c9ea86134c2e498
Vedat <vedat.arpa.72@gmail.com>
2026-01-08 00:34:07 +0300

    node.js giriş

---
+++ index.js
@@ -0,0 +1,31 @@
+// request - response => server
+
+// http 
+
+let http = require("http");//node modules => http, fs, os
+
+var fs = require("fs");
+
+var server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    fs.readFile("index.html", (err, html) => {
+      res.write(html);
+      res.end();
+    });
+  } else if (req.url == "/urunler") {
+    fs.readFile("urunler.html", (err, html) => {
+      res.write(html);
+      res.end();
+    });
+  } else {
+    fs.readFile("404.html", (err, html) => {
+      res.write(html);
+      res.end();
+    })
+  }
+
+});
+
+server.listen(3000, () => {
+  console.log("node.js server at port 3000");
+})
