7965e665271fd9b2928b69058bfc90d9f4a7b48b
Vedat <vedat.arpa.72@gmail.com>
2026-01-09 22:26:52 +0300

    express.js dersleri

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.set("view engine", "ejs");
+
+const data = [
+  { id: 1, name: "iphone 14", price: 30000, isActive: true, imageUrl: "background.jpg" },
+  { id: 2, name: "iphone 15", price: 40000, isActive: false, imageUrl: "background.jpg" },
+  { id: 3, name: "iphone 16", price: 50000, isActive: true, imageUrl: "background.jpg" },
+  { id: 4, name: "iphone 17", price: 60000, isActive: true, imageUrl: "background.jpg" }
+]
 
 // routes
 
-app.use("/products/:id/:price", function (req, res) {
-  res.sendFile("product details" + req.params.id);
+app.use("/products/:id", function (req, res) {
+  const urun = data.find(u => u.id == req.params.id);
+  res.render("product-details", urun);
 })
 
 app.use("/products", function (req, res) {
-  res.send("products");
+  res.render("products", {
+    urunler: data
+  });
 })
 
 app.use("/", function (req, res) {
-  res.send("anasayfa");
+  res.render("index");
 })
 
 app.listen(3000, () => {
